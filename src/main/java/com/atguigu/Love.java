9a20a3bda9cbb4edb62a3c0501d2b56cbb24ed73
郑帅哥 <1608200596@qq.com>
2020-09-25 16:47:03 +0800

    asd

---
+++ src/main/java/com/atguigu/Love.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author shkstart
+ * @create 2020-09-25 16:25
+ */
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("5648961");
+        System.out.println("skmfkl");
+    }
+}
